4de2b5bd29b8d2c7694ab1dd1af64ea1bd458640
Carl Thomas <cthomas4293@hotmail.com>
2020-10-28 11:13:22 -0400

    updated the .py file to convert img fomat to png

--- ImageProcessor/JPGtoPNG/jpgtopng.py
+++ ImageProcessor/JPGtoPNG/jpgtopng.py
@@ -1,5 +1,17 @@
 from PIL import Image
+import sys
+import os
 
-img = Image.open('../Images/pikachu.jpg')
+input_folder = sys.argv[1]
+output_folder = sys.argv[2]
 
-img.show()
+if not os.path.exists(output_folder):
+	os.mkdir(output_folder)
+
+for filename in os.listdir(input_folder):
+	img = Image.open(f"{input_folder}{filename}")
+	clean_img = os.path.splitext(filename)
+	img.save(f"{output_folder}{clean_img[0]}.png", "png")
+	print("All Done!")
+
+
